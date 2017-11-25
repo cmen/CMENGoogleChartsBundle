ebbce7af07d37b4cb5579c3405887e285668aca6
Christophe Meneses <christophe.meneses@gmail.com>
2017-11-25 16:55:16 +0100

    Refactoring enableInteractivity

--- GoogleCharts/Options/BasicChartOptions.php
+++ GoogleCharts/Options/BasicChartOptions.php
@@ -7,24 +7,5 @@ namespace CMEN\GoogleChartsBundle\GoogleCharts\Options;
  */
 class BasicChartOptions extends ChartOptions
 {
-    /**
-     * Whether the chart throws user-based events or reacts to user interaction. If false, the chart will not throw
-     * 'select' or other interaction-based events (but will throw ready or error events), and will not display
-     * hovertext or otherwise change depending on user input.
-     *
-     * @var boolean
-     */
-    protected $enableInteractivity;
-
-    /**
-     * @param boolean $enableInteractivity
-     *
-     * @return $this
-     */
-    public function setEnableInteractivity($enableInteractivity)
-    {
-        $this->enableInteractivity = $enableInteractivity;
-
-        return $this;
-    }
+    use EnableInteractivityTrait;
 }
---
+++ GoogleCharts/Options/EnableInteractivityTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace CMEN\GoogleChartsBundle\GoogleCharts\Options;
+
+/**
+ * @author Christophe Meneses
+ */
+trait EnableInteractivityTrait
+{
+    /**
+     * Whether the chart throws user-based events or reacts to user interaction. If false, the chart will not throw
+     * 'select' or other interaction-based events (but will throw ready or error events), and will not display
+     * hovertext or otherwise change depending on user input.
+     *
+     * Default: true
+     *
+     * @var boolean
+     */
+    protected $enableInteractivity;
+
+    /**
+     * @param boolean $enableInteractivity
+     *
+     * @return $this
+     */
+    public function setEnableInteractivity($enableInteractivity)
+    {
+        $this->enableInteractivity = $enableInteractivity;
+
+        return $this;
+    }
+}
