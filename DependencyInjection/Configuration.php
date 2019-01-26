13f43c7aebae1fc5f0ac6518d90ef2a9db90eb16
Christophe Meneses <christophe.meneses@gmail.com>
2019-01-26 16:27:20 +0100

    Remove TreeBuilder deprecation

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -12,8 +12,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('cmen_google_charts');
+        $treeBuilder = new TreeBuilder('cmen_google_charts');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('cmen_google_charts');
+        }
 
         $rootNode
             ->children()
